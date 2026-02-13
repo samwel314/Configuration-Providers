d71a9a06ec4f487aba5eca1d20ffdd87faf10d4a
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-02-14 01:54:38 +0200

    1 System Environment Variables

--- Configuration_Providers/Program.cs
+++ Configuration_Providers/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+//   this endpoint take a ( key ) and return value 
+app.MapGet("/{key}", (string key , IConfiguration config ) =>
+{
+    var value = config[key];
+    return "Key: " + key + " Value: " + value;
+});
 app.Run();
+
+/*
+ 
+ 1 system environment variables 
+  from cli >> setx key value    
+
+BEFORE USE it you should set it 
+ 
+ */
